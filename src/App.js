6129fd35c4e706cae789033d34b7e1c340f9d0a6
Nelson Anane <nelsonanane@Nelsons-iMac.fios-router.home>
2019-05-16 08:56:41 -0400

    finsh

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function transform(offset) {
+  const cos = Math.cos(offset);
+  const sin = Math.sin(offset);
+  return { transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)` };
 }
 
-export default App;
+export default class App extends Component {
+
+  state = {
+    styleOne: {},
+    styleTwo: {}
+  }
+
+  onMouseMove = (event) => {
+    this.setState({
+      styleOne: transform(-event.clientX / event.clientY),
+      styleTwo: transform(event.clientX / event.clientY)
+    })
+  }
+  render() {
+    return (
+      <div onMouseMove={this.onMouseMove}>
+        <div className="panel" style={this.state.styleOne} />
+        <div className="panel" style={this.state.styleTwo} />
+      </div>
+    )
+  }
+}
+
